165d5f16aab63dd3e56028058e717e094c33a154
Ryan Hazama <ryanhazama@Ryans-Laptop.local>
2021-12-13 20:53:33 -0800

    updating quizOver function and adjusted to other functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,81 +1,3 @@
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
-let shuffledQuestions, currentQuestionIndex
-
-startButton.addEventListener('click', startGame)
-nextButton.addEventListener('click', () => {
-    currentQuestionIndex++
-    setNextQuestion()
-})
-
-function startGame() {
-    console.log('started')
-    startButton.classList.remove('hide')
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
-    questionContainerElement.classList.remove('hide')
-    setNextQuestion()
-}
-
-function setNextQuestion() {
-    resetQuestion()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
-}
-
-function showQuestion(question) {
-    questionElement.innerText = question.question
-    question.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.textbutton.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })
-}
-
-function resetState() {
-    clearStatusClass(document.body)
-    nextButton.classList.add('hide')
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild
-        (answerButtonsElement.firstChild)
-    }
-}
-
-function selectAnswer() {
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button.dataset.correct)
-    })
-    if (shuffledQuestions.length > currentQuestionIndex +1) {
-    nextButton.classList.remove('hide')
-    } else {
-        startButton.innerText = 'Restart'
-        startButton.classList.remove('hide')
-    }
-}
-
-function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add('correct')
-    } else {
-        element.classList.add('wrong')
-    }
-}
-
-function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
-}
-
 const questions = [
     {
         question: 'Commonly used data types DO NOT include:',
@@ -99,13 +21,13 @@ const questions = [
         question: 'Arrays in Javascript can be used to store ____.',
         answers: [
             { text: 'numbers and strings', correct: true},
-            { text: 'other arrrays', correct: false},
+            { text: 'other arrays', correct: false},
             { text: 'booleans', correct: false},
             { text: 'all of the above', correct: false}
         ]
     },
     {
-        question: 'Stringe values must be enclosed within ____ when being assiged to variables',
+        question: 'String values must be enclosed within ____ when being assiged to variables',
         answers: [
             { text: 'commas', correct: true},
             { text: 'curly brackets', correct: false},
@@ -123,3 +45,133 @@ const questions = [
         ]
     }
 ]
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+const controlDiv = document.querySelector('.controls')
+const headOfDisplay = document.querySelector('.headOfDisplay-item')
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+var timeEl = document.querySelector('#time');
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    console.log('started')
+    controlDiv.classList.add('hide')
+    startButton.classList.remove('hide')
+    headOfDisplay.classList.remove('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    questionContainerElement.classList.remove('hide')
+    timerId = setInterval(clockTick, 1000)
+    timeEl.textContent = time;
+    setNextQuestion()
+}
+
+function clockTick() {
+    time--;
+    timeEl.textContent = time;
+    console.log(time)
+    if(time <= 0) {
+        console.log('quiz Over')
+    }
+}
+
+function setNextQuestion() {
+    showQuestion(questions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer.text
+        button.setAttribute('value', answer.correct)
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        questionElement.appendChild(button)
+    })
+}
+
+function setStatusClass(element, correct) {
+    console.log(element, correct)
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    console.log(element)
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.getAttribute('value')
+    if(correct === 'false') {
+        // minus time
+        time-= 15;
+        if(time <0) {
+            time = 0;
+        }
+        timeEl.textContent = time;
+    } 
+    currentQuestionIndex ++;
+    console.log(correct)
+    if(currentQuestionIndex === questions.length) {
+        quizOver()
+    } else {
+        setNextQuestion()
+    }
+}
+
+function quizOver() {
+    clearInterval(timerId);
+    console.log('quizOver')
+    // Hide the questions container by adding a hide class
+    questionContainerElement.classList.add('hide')
+    // Show the End Screen with the form to fill out highscore with initials
+    // Highscore can just be time left
+
+    // Add Save HighScore Function
+    // Set a var to the value of the input field
+    var highScores = JSON.parse(window.localStorage.getItem('highscores')) || [];
+    var newScore = {
+        score: time,
+        initials: initials
+    }
+    // push new score into highscores array
+    // set localstorage with the new highscores array
+    submitEl.addEventListener("click", function () {
+        var initials = inputEl.value;
+
+        if (initials === null) {
+            alert("Please write something here");
+        } else {
+            var totalScore = {
+                initials: initials,
+                score: time
+            }
+        }
+        // replace window location with highscores html
+        window.location.replace("./highscore.html")
+    });
+}
